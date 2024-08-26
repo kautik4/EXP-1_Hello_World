5076b89acbb25f9d11204e01c4291db364b08a5a
kautik <78012455+kautik4@users.noreply.github.com>
2024-08-26 20:03:11 +0530

    Add files via upload

---
+++ Calculator.cpp
@@ -0,0 +1,30 @@
+//Kautik Verma
+//PRN:23070123073
+//EnTC A3
+#include <iostream>
+using namespace std;
+int main(){
+     float division,a,b,sum,difference,product;
+     cout<<"Enter two numbers: ";
+     cin>>a>>b;
+     sum = a+b;
+     difference = a-b;
+     product = a*b;
+     division = a/b;
+     cout<<"Sum is: "<<sum<<endl;
+     cout<<"Difference is: "<<difference<<endl;
+     cout<<"Product is: "<<product<<endl;
+     cout<<"Division is: "<<division<<endl;
+
+}
+
+/*Output:
+Enter two numbers: 34
+23
+Sum is: 57
+Difference is: 11
+Product is: 782
+Division is: 1.47826
+*/
+
+   
